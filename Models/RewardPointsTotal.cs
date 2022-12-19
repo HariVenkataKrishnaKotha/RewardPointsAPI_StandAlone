f966e2108a5546cb0af38e42edd7023349d07ffb
hvkkotha <hk00016@mix.wvu.edu>
2022-12-19 01:25:14 -0500

    Added Model Classes for the data models used by the API.

--- Models/RewardPointsTotal.cs
+++ Models/RewardPointsTotal.cs
@@ -1,6 +1,13 @@
-﻿namespace RewardPointsAPI_StandAlone.Models
+﻿using System;
+using System.Globalization;
+
+namespace RewardPointsAPI_SA.Models
 {
+    // RewardPointsTotal.cs
+
     public class RewardPointsTotal
     {
+        public string? Customer { get; set; }
+        public int Points { get; set; }
     }
 }
--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -1,6 +1,14 @@
-﻿namespace RewardPointsAPI_StandAlone.Models
+﻿using System;
+using System.Globalization;
+
+namespace RewardPointsAPI_SA.Models
 {
+    // Transaction.cs
+
     public class Transaction
     {
+        public string? Customer { get; set; }
+        public int Month { get; set; }
+        public int Amount { get; set; }
     }
 }
